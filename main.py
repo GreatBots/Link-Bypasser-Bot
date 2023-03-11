ce8ccc2c16c1bcd10044f58d7d74c26d93081785
Vivek Tp <Vivektvp1@gmail.com>
2023-03-11 08:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ import requests
 import threading
 from texts import HELP_TEXT
 from bypasser import ddllist
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-
-MUST_JOIN = "MyOwnBots"
 
 # bot
 app = Client(
@@ -20,35 +17,6 @@ app = Client(
     api_id=int(os.environ["API_ID"]),
     api_hash=os.environ["API_HASH"],
 )
-
-# forcesub
-@app.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(bot: Client, msg: Message):
-    if not MUST_JOIN:  # Not compulsory
-        return
-    try:
-        try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await bot.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply(
-                    f"**please Please Join My Updates Channel To Use Me! And click on to Check /start !**",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("Join Updates Channel", url=link)]
-                    ])
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
-
 
 # handle ineex
 def handleIndex(ele,message,msg):
